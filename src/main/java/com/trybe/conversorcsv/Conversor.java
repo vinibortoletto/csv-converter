27271258869eb6553c317b204b802493ef57d146
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-05-12 23:36:50 -0300

    feat: formata dados da linha

--- src/main/java/com/trybe/conversorcsv/Conversor.java
+++ src/main/java/com/trybe/conversorcsv/Conversor.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.Buffer;
+import java.util.Arrays;
 
 public class Conversor {
 
@@ -30,6 +31,63 @@ public class Conversor {
     }
   }
 
+  private String formataData(String data) {
+    String[] dataDividida = data.split("/");
+    String dia = dataDividida[0];
+    String mes = dataDividida[1];
+    String ano = dataDividida[2];
+    String novaData = String.format("%s-%s-%s", ano, mes, dia); 
+    return novaData;
+  }
+
+  private String formataCpf(String cpf) {
+    String cpf1 = cpf.substring(0, 3);
+    String cpf2 = cpf.substring(3, 6);
+    String cpf3 = cpf.substring(6, 9);
+    String cpf4 = cpf.substring(9);
+    String novoCpf = String.format("%s.%s.%s-%s", cpf1, cpf2, cpf3, cpf4);
+
+    return novoCpf;
+  }
+
+
+  private String manipulaLinha(String linha) {
+    String[] novaLinha = linha.split(","); 
+    String nome = novaLinha[0].toUpperCase();
+    String data = this.formataData(novaLinha[1]);
+    String email = novaLinha[2];
+    String cpf = this.formataCpf(novaLinha[3]);
+
+    return String.format("%s,%s,%s,%s", nome, data, email, cpf);
+  }
+
+  private void leArquivo(File pastaDeEntradas) {
+    if (pastaDeEntradas.isDirectory() && pastaDeEntradas.canRead()) {
+      for (File arquivo : pastaDeEntradas.listFiles()) {
+        FileReader fileReader = null;
+        BufferedReader bufferedReader = null;
+        
+        try {
+          fileReader = new FileReader(arquivo);
+          bufferedReader = new BufferedReader(fileReader);
+          String linha = bufferedReader.readLine();
+          
+          while (linha != null) {
+            if (!linha.contains("Nome")) {
+              String novaLinha = this.manipulaLinha(linha);
+            }
+
+            linha = bufferedReader.readLine();
+          }
+        } catch (IOException error) {
+          error.printStackTrace();
+        } finally {
+          this.fecharAquivo(fileReader, bufferedReader);
+        }
+      }
+    }
+  }
+
   /**
    * Converte todos os arquivos CSV da pasta de entradas. Os resultados são gerados
    * na pasta de saídas, deixando os arquivos originais inalterados.
@@ -42,28 +100,8 @@ public class Conversor {
    *                     gravar os arquivos de saída.
    */
   public void converterPasta(File pastaDeEntradas, File pastaDeSaidas) throws IOException {
+    this.leArquivo(pastaDeEntradas);
     
     
-    if (pastaDeEntradas.isDirectory() && pastaDeEntradas.canRead()) {
-      for (File arquivo : pastaDeEntradas.listFiles()) {
-        FileReader fileReader = null;
-        BufferedReader bufferedReader = null;
-        
-        try {
-          fileReader = new FileReader(arquivo);
-          bufferedReader = new BufferedReader(fileReader);
-
-          String conteudoDaLinha = bufferedReader.readLine();
-          
-          while (conteudoDaLinha != null) {
-            conteudoDaLinha = bufferedReader.readLine();
-          }
-        } catch (IOException error) {
-          error.printStackTrace();
-        } finally {
-          this.fecharAquivo(fileReader, bufferedReader);
-        }
-      }
-    }
   }
 }
