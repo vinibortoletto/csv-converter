e7a3d7bfa9f37bf826399518a72f9be6e9c75420
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-05-12 22:53:45 -0300

    feat: read files

--- src/main/java/com/trybe/conversorcsv/Conversor.java
+++ src/main/java/com/trybe/conversorcsv/Conversor.java
@@ -1,7 +1,10 @@
 package com.trybe.conversorcsv;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.nio.Buffer;
 
 public class Conversor {
 
@@ -18,6 +21,15 @@ public class Conversor {
     new Conversor().converterPasta(pastaDeEntradas, pastaDeSaidas);
   }
 
+  private void fecharAquivo(FileReader fileReader, BufferedReader bufferedReader) {
+    try {
+      fileReader.close();
+      bufferedReader.close();
+    } catch (Exception error) {
+      error.printStackTrace();
+    }
+  }
+
   /**
    * Converte todos os arquivos CSV da pasta de entradas. Os resultados são gerados
    * na pasta de saídas, deixando os arquivos originais inalterados.
@@ -30,6 +42,28 @@ public class Conversor {
    *                     gravar os arquivos de saída.
    */
   public void converterPasta(File pastaDeEntradas, File pastaDeSaidas) throws IOException {
-    // TODO: Implementar.
+    
+    
+    if (pastaDeEntradas.isDirectory() && pastaDeEntradas.canRead()) {
+      for (File arquivo : pastaDeEntradas.listFiles()) {
+        FileReader fileReader = null;
+        BufferedReader bufferedReader = null;
+        
+        try {
+          fileReader = new FileReader(arquivo);
+          bufferedReader = new BufferedReader(fileReader);
+
+          String conteudoDaLinha = bufferedReader.readLine();
+          
+          while (conteudoDaLinha != null) {
+            conteudoDaLinha = bufferedReader.readLine();
+          }
+        } catch (IOException error) {
+          error.printStackTrace();
+        } finally {
+          this.fecharAquivo(fileReader, bufferedReader);
+        }
+      }
+    }
   }
 }
